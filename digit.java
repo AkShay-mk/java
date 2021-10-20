ceb0c09d51af193c89455d3dfa531eff838cfb5c
AkShay-mk <89240105+AkShay-mk@users.noreply.github.com>
2021-10-20 09:19:21 -0700

    Add files via upload

---
+++ digit.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class digit 
+{
+
+	public static void main(String[] args) 
+	{
+		int count=0;
+		for(int i=1;i<=20;i=i+1)
+		{
+			count=count+1;
+			 
+		}
+		System.out.println(count); 
+	}
+	    
+}
---
+++ prime.java
@@ -0,0 +1,30 @@
+package sample;
+
+public class prime {
+
+	public static void main(String[] args) 
+	{
+		int num=100;
+		int count=0;
+		for(int i=1;i<=num;i=i+1)
+		{
+			if(num % i==0)
+			{
+				count=count+1;
+				
+			}
+		}
+		if(count==2)
+		{
+			System.out.println("prime number");
+			
+		}
+		else
+		{
+			System.out.println("not prime");
+		}
+		
+
+	}
+
+}
